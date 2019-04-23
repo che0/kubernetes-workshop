61b5baddf3a1c96bd25e05f919370c972aadb66e
Petr Novak 2 <petr.novak2@firma.seznam.cz>
2019-04-23 18:47:04 +0200

    7: Ingress

---
+++ setup-scripts/release-ip.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcloud compute --project=pokus-234611 addresses delete my-static-address --global
---
+++ setup-scripts/reserve-ip.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcloud compute --project=pokus-234611 addresses create my-static-address --global --network-tier=PREMIUM
