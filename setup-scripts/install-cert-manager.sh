df45e4e2d51fe2d9920160b4ccc8f57864ddc030
Petr Novak 2 <petr.novak2@firma.seznam.cz>
2019-04-24 12:37:11 +0200

    ingress with tls

--- setup-scripts/install-cert-manager.sh
+++ setup-scripts/install-cert-manager.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -ex
 
-# Podle https://docs.cert-manager.io/en/latest/getting-started/install.html ale upraveno na fixni verzi 0.7.0
-# (deploy s https://raw.githubusercontent.com/jetstack/cert-manager/release-0.7/deploy/manifests/cert-manager.yaml
-# jeste nemel image)
+# Podle https://docs.cert-manager.io/en/latest/getting-started/install.html 
+# * upraveno na fixni verzi 0.7.0 (deploy s https://raw.githubusercontent.com/jetstack/cert-manager/release-0.7/deploy/manifests/cert-manager.yaml jeste nemel image)
+# * bez validacniho webhooku (mame privatni GKE cluster)
 
 kubectl create namespace cert-manager
 kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
 kubectl apply \
-    -f https://raw.githubusercontent.com/jetstack/cert-manager/v0.7.0/deploy/manifests/cert-manager.yaml \
+    -f https://raw.githubusercontent.com/jetstack/cert-manager/v0.7.0/deploy/manifests/cert-manager-no-webhook.yaml \
     --validate=false
--- setup-scripts/reserve-ip.sh
+++ setup-scripts/reserve-ip.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -ex
 gcloud compute --project=pokus-234611 addresses create my-static-address --global --network-tier=PREMIUM
+gcloud compute --project=pokus-234611 addresses describe my-static-address --global | grep ^address
